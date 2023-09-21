0ea4613d9d8156d63043a3a02ffb4d71f2b760e4
Moeen <moeen12yt@gmail.com>
2023-09-21 11:08:52 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Project/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'cbg': 'rgba(84, 84, 212, 0.10)',
+        'cbgr':'rgba(240, 64, 55, 0.10)',
+        'cbgy': 'rgba(254, 220, 90, 0.10)',
+      },
+      textColor: {
+        'custom': 'rgba(255, 255, 255, 0.70)',
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'nunito-sans': ['"Nunito Sans"', 'sans-serif'],
+        'graphik': ['Graphik', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
